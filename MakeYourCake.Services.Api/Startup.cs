930b55066957a79252db21a0723cce5365098af1
Lavanya Juturu <jlavanyareddy@gmail.com>
2021-11-09 17:38:07 -0800

    INFRA: Intialize APi Project

---
+++ MakeYourCake.Services.Api/Startup.cs
@@ -0,0 +1,66 @@
+// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace MakeYourCake.Services.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                var openApiInfo = new OpenApiInfo
+                {
+                    Title = "MakeYourCake.Services.Api",
+                    Version = "v1"
+                };
+
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: openApiInfo);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "MakeYourCake.ServicesApi v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}

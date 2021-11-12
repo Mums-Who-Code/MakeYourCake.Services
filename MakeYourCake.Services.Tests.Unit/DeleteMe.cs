917b366b332e51159cddd0bcc533a450036073c5
Lavanya Juturu <jlavanyareddy@gmail.com>
2021-11-12 15:00:22 -0800

    INFRA: Intialize Unit Test Project

---
+++ MakeYourCake.Services.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Xunit;
+
+namespace MakeYourCake.Services.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void DeleteMeTest()
+        {
+            Assert.True(true);
+        }
+    }
+}

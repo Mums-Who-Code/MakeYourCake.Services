5a88d1ffc673b1ec1c506abf4587d9730d006b5c
Lavanya Juturu <jlavanyareddy@gmail.com>
2021-11-23 17:32:53 -0800

    INFRA: Initialize Acceptance Project

---
+++ MakeYourCake.Services.Tests.Acceptance/Apis/Home/HomeApiTests.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using MakeYourCake.Services.Tests.Acceptance.Brokers;
+using Xunit;
+
+namespace MakeYourCake.Services.Tests.Acceptance.Apis.Home
+{
+    [Collection(nameof(ApiTestCollection))]
+    public partial class HomeApiTests
+    {
+        private readonly MakeYourCakeApiBroker makeYourCakeApiBroker;
+
+        public HomeApiTests(MakeYourCakeApiBroker makeYourCakeApiBroker) =>
+            this.makeYourCakeApiBroker = makeYourCakeApiBroker;
+    }
+}
---
+++ MakeYourCake.Services.Tests.Acceptance/Brokers/MakeYourCakeApiBroker.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Net.Http;
+using MakeYourCake.Services.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RESTFulSense.Clients;
+
+namespace MakeYourCake.Services.Tests.Acceptance.Brokers
+{
+    public partial class MakeYourCakeApiBroker
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private readonly HttpClient httpClient;
+        private readonly IRESTFulApiFactoryClient apiFactoryClient;
+
+        public MakeYourCakeApiBroker()
+        {
+            this.webApplicationFactory = new WebApplicationFactory<Startup>();
+            this.httpClient = this.webApplicationFactory.CreateClient();
+            this.apiFactoryClient = new RESTFulApiFactoryClient(this.httpClient);
+
+        }
+    }
+}
